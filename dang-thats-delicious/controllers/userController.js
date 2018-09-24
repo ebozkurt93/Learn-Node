54b82e1f7b8fba02d0c76fd4e2d1789fcfdd4616
Erdem Bozkurt <ebozkurt93@gmail.com>
2018-09-24 17:31:07 +0300

    video 25

--- dang-thats-delicious/controllers/userController.js
+++ dang-thats-delicious/controllers/userController.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 exports.loginForm = (req, res) => {
   res.render('login', { title: 'Login' });
@@ -11,18 +13,15 @@ exports.registerForm = (req, res) => {
 exports.validateRegister = (req, res, next) => {
   req.sanitizeBody('name');
   req.checkBody('name', 'You must supply a name!').notEmpty();
-  req
-    .checkBody('email', 'That Email is not valid!')
-    .notEmpty()
-    .isEmail();
+  req.checkBody('email', 'That Email is not valid!').isEmail();
   req.sanitizeBody('email').normalizeEmail({
-    remove_dots: false,
+    gmail_remove_dots: false,
     remove_extension: false,
     gmail_remove_subaddress: false
   });
   req.checkBody('password', 'Password Cannot be Blank!').notEmpty();
   req
-    .checkBody('password-confirm', 'Confirmed Password Cannot be Blank!')
+    .checkBody('password-confirm', 'Confirmed Password cannot be blank!')
     .notEmpty();
   req
     .checkBody('password-confirm', 'Oops! Your passwords do not match')
@@ -36,7 +35,14 @@ exports.validateRegister = (req, res, next) => {
       body: req.body,
       flashes: req.flash()
     });
-    return;
+    return; // stop the fn from running
   }
-  next();
+  next(); // there were no errors!
+};
+
+exports.register = async (req, res, next) => {
+  const user = new User({ email: req.body.email, name: req.body.name });
+  const register = promisify(User.register, User);
+  await register(user, req.body.password);
+  next(); // pass to authController.login
 };
